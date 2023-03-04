00c84d9dda86625559438a51c394fbf53223236c
qixiangiii <106664694+qixiangiii@users.noreply.github.com>
2023-03-04 10:39:16 +0800

    Update 自动连接校园网.js

--- 自动连接校园网.js
+++ 自动连接校园网.js
@@ -2,7 +2,7 @@
 // @name         南宁理工学院校园网自动登录脚本
 // @namespace    https://github.com/qixiangiii/Campus-network
 // @version      0.1
-// @description  南宁理工学院校园网自动登录脚本 有问题请联系qq:1462709953
+// @description  南宁理工学院校园网自动登录脚本 有问题请联系qq:1462709953    Microsoft、Chrome浏览器能自动登录  部分浏览器需要刷新即可自动登录如：QQ浏览器  
 // @author       QiXiang
 // @match        http://10.251.251.250/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
